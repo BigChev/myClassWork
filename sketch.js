db50e717a643a09b99c6d1d63447a481c817dfb9
BigChev <chevaniquef@icloud.com>
2024-12-20 21:52:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function draw() {
    //an RGB color for the canvas' background
   //circle
   stroke(255,255,5); // an RGB color for the circle's border
-  fill(255,spongebob,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  fill(255,spongebob,127,150); 
   
 
   
