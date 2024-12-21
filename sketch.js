f020223a91ffbaedd8909725c0250d7565fa7ac0
BigChev <chevaniquef@icloud.com>
2024-12-20 22:01:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,6 @@ function draw() {
   stroke(255,255,5); // an RGB color for the circle's border
   fill(255,spongebob,127,150); 
 
-
 	
   fill(250,250,250);
   stroke(0,0,0);
@@ -40,6 +39,7 @@ function draw() {
   textFont("Times New Roman");
   textSize(21);
   text("By Unknown",190,190);
+
 
 
 	
